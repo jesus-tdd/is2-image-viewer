3b535b0a34bffa6464628958de907fbbe0206fa9
Jesus <jesus.tdd03@gmail.com>
2025-01-19 12:31:57 +0000

    Created class SwingImageDisplay

---
+++ src/main/java/software/ulpgc/is2/apps/windows/view/SwingImageDisplay.java
@@ -0,0 +1,102 @@
+package software.ulpgc.is2.apps.windows.view;
+
+import software.ulpgc.is2.architecture.view.ImageDisplay;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class SwingImageDisplay extends JPanel implements ImageDisplay {
+    private Shift shift = Shift.Null;
+    private Released released = Released.Null;
+    private int initShift;
+    private List<Paint> paints = new ArrayList();
+
+    public SwingImageDisplay() {
+        this.addMouseListener(mouseListener());
+        this.addMouseMotionListener(mouseMotionListener());
+    }
+
+    private MouseMotionListener mouseMotionListener() {
+        return new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                shift.offset(e.getX() - initShift);
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {}
+        };
+    }
+
+    private MouseListener mouseListener() {
+        return new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {}
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                initShift = e.getX();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                released.offset(e.getX() - initShift);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        };
+    }
+
+    @Override
+    public void paint(String id, int offset) {
+        paints.add(new Paint(id, offset));
+        repaint();
+    }
+
+    private static final Map<String, Color> colors = Map.of(
+            "red", Color.RED,
+            "green", Color.GREEN,
+            "blue", Color.BLUE
+    );
+
+    @Override
+    public void paint(Graphics g) {
+        for (Paint paint : paints) {
+            g.getColor(colors.get(paint.id));
+            g.fillRect(paint.offset, 0, 800, 600);
+        }
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+        paints.clear();
+    }
+
+    @Override
+    public void on(Shift shift) {
+        this.shift = shift != null ? shift : Shift.Null;
+    }
+
+    @Override
+    public void on(Released released) {
+        this.released = released != null ? released : Released.Null;
+    }
+
+    private record Paint(String id, int offset) {
+    }
+}
