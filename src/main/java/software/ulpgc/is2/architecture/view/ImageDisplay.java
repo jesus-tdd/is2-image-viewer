e37d944435cc4ce0c4170d1276af39ea6d34d7e5
Jesus <jesus.tdd03@gmail.com>
2025-01-19 11:56:11 +0000

    Created class ImageDisplay

---
+++ src/main/java/software/ulpgc/is2/architecture/view/ImageDisplay.java
@@ -0,0 +1,19 @@
+package software.ulpgc.is2.architecture.view;
+
+public interface ImageDisplay {
+    void paint(String id, int offset);
+    int getWidth();
+    void clear();
+    void on(Shift shift);
+    void on(Released released);
+
+    interface Shift {
+        Shift Null = offset -> {};
+        void offset(int offset);
+    }
+
+    interface Released {
+        Released Null = offset -> {};
+        void offset(int offset);
+    }
+}
